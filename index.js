1449a57dd9d3664eae41c85722c98f6050cd51d3
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-10-25 14:32:47 +0600

    courses_data_added

--- index.js
+++ index.js
@@ -5,9 +5,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
+// app.get('/', (req, res) => {
+//     res.send('Server is running');
+// })
+
+
 app.get('/', (req, res) => {
-    res.send('Server is running');
-})
+    res.send(courses);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
